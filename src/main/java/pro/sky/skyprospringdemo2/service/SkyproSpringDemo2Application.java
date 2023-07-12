5f649fd97a09f70be20557c3e4775374d6e5ff3d
79998 <lekha-fadeev@mail.ru>
2023-07-12 06:02:14 +0300

    коммит

---
+++ src/main/java/pro/sky/skyprospringdemo2/service/SkyproSpringDemo2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringdemo2.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringDemo2Application.class, args);
+	}
+
+}
